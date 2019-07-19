82bd024a4fff6daf15368d0a825bbffaed8c93e8
Nathan Casals <ncasals@semeia.io>
2019-07-19 16:29:34 +0200

    ✨Change dataset location param to dataset

--- prepare_mixmatch.sh
+++ prepare_mixmatch.sh
@@ -4,19 +4,19 @@ while getopts d:n: option
 do
 case "${option}"
 in
-d) DATA_DIR=${OPTARG};;
+d) DATA_SET=${OPTARG};;
 n) NB_SAMPLES=${OPTARG};;
 esac
 done
 
 #the code below is based on mixmatch's README
 export PYTHONPATH=$PYTHONPATH:.
-export ML_DATA=$DATA_DIR
+export ML_DATA='Data'
 
 if [[ -z "$NB_SAMPLES" ]] ; then
-    CUDA_VISIBLE_DEVICES= ./scripts/create_datasets.py --dataset=mnist
+    CUDA_VISIBLE_DEVICES= ./scripts/create_datasets.py --dataset=$DATA_SET
 else
-    CUDA_VISIBLE_DEVICES= ./scripts/create_datasets.py --dataset=mnist --nb_samples=$NB_SAMPLES
+    CUDA_VISIBLE_DEVICES= ./scripts/create_datasets.py --dataset=$DATA_SET --nb_samples=$NB_SAMPLES
 fi
 
 # Download datasets
@@ -29,7 +29,7 @@ fi
 #         CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=$seed --size=$size $ML_DATA/SSL/svhn $ML_DATA/svhn-train.tfrecord $ML_DATA/svhn-extra.tfrecord &
 #         CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=$seed --size=$size $ML_DATA/SSL/svhn_noextra $ML_DATA/svhn-train.tfrecord &
 #         CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=$seed --size=$size $ML_DATA/SSL/cifar10 $ML_DATA/cifar10-train.tfrecord &
-    CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=3 --size=$size $ML_DATA/SSL/mnist $ML_DATA/mnist-train.tfrecord
+    CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=3 --size=$size $ML_DATA/SSL/$DATA_SET $ML_DATA/$DATA_SET-train.tfrecord
 # done
      # CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=$seed --size=10000 $ML_DATA/SSL/cifar100 $ML_DATA/cifar100-train.tfrecord &
      # CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=$seed --size=1000 $ML_DATA/SSL/stl10 $ML_DATA/stl10-train.tfrecord $ML_DATA/stl10-unlabeled.tfrecord &
