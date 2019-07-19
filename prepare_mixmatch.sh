0eaca45eccab23c16d04358d082a9055570040b3
Nathan Casals <ncasals@semeia.io>
2019-07-19 16:34:19 +0200

    🎨 Better syntax for variables

--- prepare_mixmatch.sh
+++ prepare_mixmatch.sh
@@ -10,13 +10,13 @@ esac
 done
 
 #the code below is based on mixmatch's README
-export PYTHONPATH=$PYTHONPATH:.
+export PYTHONPATH=${PYTHONPATH}:.
 export ML_DATA='Data'
 
 if [[ -z "$NB_SAMPLES" ]] ; then
-    CUDA_VISIBLE_DEVICES= ./scripts/create_datasets.py --dataset=$DATA_SET
+    CUDA_VISIBLE_DEVICES= ./scripts/create_datasets.py --dataset=${DATA_SET}
 else
-    CUDA_VISIBLE_DEVICES= ./scripts/create_datasets.py --dataset=$DATA_SET --nb_samples=$NB_SAMPLES
+    CUDA_VISIBLE_DEVICES= ./scripts/create_datasets.py --dataset=${DATA_SET} --nb_samples=${NB_SAMPLES}
 fi
 
 # Download datasets
@@ -29,7 +29,7 @@ fi
 #         CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=$seed --size=$size $ML_DATA/SSL/svhn $ML_DATA/svhn-train.tfrecord $ML_DATA/svhn-extra.tfrecord &
 #         CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=$seed --size=$size $ML_DATA/SSL/svhn_noextra $ML_DATA/svhn-train.tfrecord &
 #         CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=$seed --size=$size $ML_DATA/SSL/cifar10 $ML_DATA/cifar10-train.tfrecord &
-    CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=3 --size=$size $ML_DATA/SSL/$DATA_SET $ML_DATA/$DATA_SET-train.tfrecord
+    CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=3 --size=${size} ${ML_DATA}/SSL/${DATA_SET} ${ML_DATA}/${DATA_SET}-train.tfrecord
 # done
      # CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=$seed --size=10000 $ML_DATA/SSL/cifar100 $ML_DATA/cifar100-train.tfrecord &
      # CUDA_VISIBLE_DEVICES= scripts/create_split.py --seed=$seed --size=1000 $ML_DATA/SSL/stl10 $ML_DATA/stl10-train.tfrecord $ML_DATA/stl10-unlabeled.tfrecord &
