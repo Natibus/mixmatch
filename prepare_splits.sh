d639747fdf3d9bb05cbc64a4357824c7cb44bc09
Nathan Casals <ncasals@semeia.io>
2019-07-25 11:23:16 +0200

    🐛Add args to prepare splits script

--- prepare_splits.sh
+++ prepare_splits.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+usage="$(basename "$0") [-h] [-d dataset] [-n nb_samples] -- Script used to prepare datasets for mixmatch training
+
+where:
+    -h  show this help text
+    -d  set the dataset name (cifar10 or mnist)
+    "
+
+while getopts 'h?d:n:' option; do
+    case "${option}" in
+        h) echo "$usage"
+           exit
+           ;;
+        d) DATA_SET=${OPTARG};;
+    esac
+done
+
 # Create semi-supervised subsets ( you can use different seeds like in the readme )
 
 # The code below is based on mixmatch's README
